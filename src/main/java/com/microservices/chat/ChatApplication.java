d34d8addb10eae9a8aab00cc13012697f080b37c
Kevin Decena <kcdecena.16@gmail.com>
2024-03-19 11:38:30 +0800

    Started new chat project

---
+++ src/main/java/com/microservices/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
